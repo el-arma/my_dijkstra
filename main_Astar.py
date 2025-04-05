cc2bde9e957aa2a34f448cbd32d1799f48cdb452
el-arma <>
2025-04-06 00:58:43 +0200

    Astar implementation
    Astar added to pathfinders. Separate main with
    Astart prezentation added.

---
+++ main_Astar.py
@@ -0,0 +1,54 @@
+import osmnx as ox
+from pathfinders import Astar_algo, euclidian_dist
+from utils import create_graph_dict, list_route
+
+def route_planner(country, city, start_co, dest_co):
+
+    specific_city = f"{city}, {country}"
+    # get graph for a given area
+
+    print("Please wait downloading map (usually takes around 40 seconds)...")
+    G = ox.graph_from_place(specific_city, network_type = "drive")
+    # download routs
+
+    G_proj = ox.project_graph(G)
+    # graph projection (special for Astar heuristic)
+
+    # reversed (y, x) to be in line with google maps
+    orig = ox.distance.nearest_nodes(G, start_co[1], start_co[0]) 
+    dest = ox.distance.nearest_nodes(G, dest_co[1], dest_co[0])  
+
+    # extract nodes and edges:
+    edges = ox.graph_to_gdfs(G, nodes = True, edges = True)
+
+    graph = create_graph_dict(edges)
+
+    print("Finding optimal route...")
+    route, dist = Astar_algo(graph, G_proj, orig, dest)
+
+    # Draw the route
+    ox.plot_graph_routes(G, [route], route_linewidth = 4, node_size = 0)
+
+    print(f"Distance to target: {dist/1_000:.2f}km")
+  
+    itinerary = list_route(G, route)
+
+    print(itinerary)
+
+    return None
+
+if __name__ == "__main__": 
+
+    # Case I: KRAKÓW:
+    start_coordinates = (50.06798, 19.91234) # Informatyka AGH
+
+    destination_coordinates = (50.07680, 20.03280)  # Ośrodek Kultury Kraków-Nowa Huta
+
+    route_planner(country = "Polska", city = "Kraków", start_co = start_coordinates , dest_co = destination_coordinates)
+
+    # Case II: WARSZAWA
+    start_coordinates = (52.24136, 21.03236) # Pomnik Syreny nad Wisłą
+ 
+    destination_coordinates = (52.16450, 21.08910)  # Muzeum Pałacu Króla Jana III w Wilanowie
+
+    route_planner(country = "Polska", city = "Warszawa", start_co = start_coordinates , dest_co = destination_coordinates)
--- pathfinders.py
+++ pathfinders.py
@@ -1,7 +1,8 @@
 import heapq as hq
-from typing import Dict
+from typing import Dict, Callable, Tuple
+from math import sqrt
 
-def dijkstra_algo(graph: Dict, start_node: str, end_node: str):
+def dijkstra_algo(graph: Dict, start_node: str, end_node: str) -> Tuple:
 
     """
     Implements Dijkstra's algorithm to find the shortest path in a weighted graph.
@@ -101,3 +102,99 @@ def dijkstra_algo(graph: Dict, start_node: str, end_node: str):
     # Return the shortest distance to the end_node and the reconstructed path
 
     return path, distances[end_node]
+
+
+def euclidian_dist(G_proj, beg_node_osmid: int, end_node_osmid: int) -> float:
+# reversed (y, x) to be in line with google maps
+
+    x1, y1 = G_proj.nodes[beg_node_osmid]["x"], G_proj.nodes[beg_node_osmid]["y"]
+    x2, y2 = G_proj.nodes[end_node_osmid]["x"], G_proj.nodes[end_node_osmid]["y"]
+
+    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    # returns distance in [m]
+
+
+def Astar_algo(graph: Dict,
+               graph_proj, 
+               start_node: str, 
+               end_node: str, 
+               heuristic_func: Callable = euclidian_dist) -> Tuple:
+    """
+    Implements A* algorithm to find the shortest path in a weighted graph.
+
+    Parameters:
+    graph (dict): A dictionary representing the graph where keys are nodes and values are dictionaries
+                  of neighboring nodes with their edge weights.
+    graph_proj (MultiDiGraph): Projected graph needed to calculate heuristic function.
+    start_node (str): The starting node for the algorithm.
+    end_node (str): The target node for which the shortest path is calculated.
+    heuristic_func (callable): A function that takes two node labels (current, goal) and returns
+                               an estimated cost from current to goal. By default euclidian distance.
+
+    Returns:
+    tuple: A tuple containing the shortest distance to the end_node and the path taken.
+    """
+
+    # Initialize the shortest distances from the starting node to all other nodes as infinity
+    g_scores = {v: float("inf") for v in graph.keys()}
+
+
+    # f_scores: estimated total cost from start to goal through the node
+    f_scores = {v: float("inf") for v in graph.keys()}
+    f_scores[start_node] = heuristic_func(graph_proj, start_node, end_node)
+
+    # Previous node to reconstruct path
+    previous_nodes = {v: None for v in graph.keys()}
+
+
+
+
+    # Set the distance to the starting node as 0 since it is the starting point
+    g_scores[start_node] = 0
+
+    # Priority queue to store nodes along with their current shortest distance (f_score, node)
+    pq = []
+
+    hq.heappush(pq, (f_scores[start_node], start_node))
+    # Push the starting node with distance 0 into the priority queue
+
+    while pq:
+        _, current_node = hq.heappop(pq)
+
+        if current_node == end_node:
+            break
+        
+        try:
+            for neighbor, weight in graph[current_node].items():
+                tentative_g_score = g_scores[current_node] + weight
+                
+                if tentative_g_score < g_scores.get(neighbor, float("inf")):
+                    g_scores[neighbor] = tentative_g_score
+                    f_scores[neighbor] = tentative_g_score + heuristic_func(graph_proj, neighbor, end_node)
+                    previous_nodes[neighbor] = current_node
+                    hq.heappush(pq, (f_scores[neighbor], neighbor))
+                
+        except KeyError:
+                # protection against false neighours nodes, which can happen in real life data
+                pass
+
+    # Reconstruct the shortest path from the end_node to the start_node
+    path = []
+
+    reverse_current_node = end_node
+
+    while reverse_current_node is not None:  
+    # Traverse back using the previous_nodes dictionary
+    # from End to start
+
+        path.append(reverse_current_node)
+
+        reverse_current_node = previous_nodes[reverse_current_node]
+        
+    path.reverse()  # Reverse the path to get it from start_node to end_node
+
+    # Return the shortest distance to the end_node and the reconstructed path
+
+    return path, g_scores[end_node]
+
+
