c7d173f915f930c55cd0ef823c21b4f2f4a15d4f
el-arma <>
2025-04-06 10:36:46 +0200

    Astar polishing

--- pathfinders.py
+++ pathfinders.py
@@ -2,7 +2,7 @@ import heapq as hq
 from typing import Dict, Callable, Tuple
 from math import sqrt
 
-def dijkstra_algo(graph: Dict, start_node: str, end_node: str) -> Tuple:
+def dijkstra_algo(graph: Dict, start_node: int, end_node: int) -> Tuple:
 
     """
     Implements Dijkstra's algorithm to find the shortest path in a weighted graph.
@@ -81,7 +81,10 @@ def dijkstra_algo(graph: Dict, start_node: str, end_node: str) -> Tuple:
                     hq.heappush(pq, (new_distance, neighbor_node))
 
             except KeyError:
-            # protection against false neighours nodes, which can happen in real life data
+            # Protection against false neighbor nodes, which can occur in real-life data due to:
+            # - Missing or incomplete edges in the graph representation.
+            # - Nodes being referenced as neighbors but not defined in the graph structure.
+            # This ensures the algorithm continues without crashing when encountering such inconsistencies.
                 pass
 
     # Reconstruct the shortest path from the end_node to the start_node
@@ -116,8 +119,8 @@ def euclidian_dist(G_proj, beg_node_osmid: int, end_node_osmid: int) -> float:
 
 def Astar_algo(graph: Dict,
                graph_proj, 
-               start_node: str, 
-               end_node: str, 
+               start_node: int, 
+               end_node: int, 
                heuristic_func: Callable = euclidian_dist) -> Tuple:
     """
     Implements A* algorithm to find the shortest path in a weighted graph.
@@ -128,8 +131,9 @@ def Astar_algo(graph: Dict,
     graph_proj (MultiDiGraph): Projected graph needed to calculate heuristic function.
     start_node (str): The starting node for the algorithm.
     end_node (str): The target node for which the shortest path is calculated.
-    heuristic_func (callable): A function that takes two node labels (current, goal) and returns
-                               an estimated cost from current to goal. By default euclidian distance.
+    heuristic_func (callable): A function that takes the graph projection and two node IDs 
+                               (current, goal) and returns an estimated cost from current to goal. 
+                               By default, it uses the Euclidean distance.
 
     Returns:
     tuple: A tuple containing the shortest distance to the end_node and the path taken.
@@ -143,10 +147,15 @@ def Astar_algo(graph: Dict,
     f_scores = {v: float("inf") for v in graph.keys()}
     f_scores[start_node] = heuristic_func(graph_proj, start_node, end_node)
 
-    # Previous node to reconstruct path
+    # Initialize the previous node dictionary to reconstruct the shortest path
     previous_nodes = {v: None for v in graph.keys()}
 
-
+    # Example:
+    #  |Prev. node
+    #---------
+    # A| None
+    # B| None
+    # C| None
 
 
     # Set the distance to the starting node as 0 since it is the starting point
@@ -158,24 +167,45 @@ def Astar_algo(graph: Dict,
     hq.heappush(pq, (f_scores[start_node], start_node))
     # Push the starting node with distance 0 into the priority queue
 
-    while pq:
-        _, current_node = hq.heappop(pq)
+    while pq:  
+    # Continue until the priority queue is empty
+        
+        # Pop the node with the smallest distance from the priority queue
+        # and assign to current_distance and current node
+        f_score_dist, current_node = hq.heappop(pq)
+        # Example: when we start we remove start_node: "A"
 
+        # If we have reached the target node, we can stop the algorithm
         if current_node == end_node:
             break
+
+        # Skip the current item if the current distance is greater than the recorded shortest distance
+        if f_score_dist > f_scores[current_node]:
+            continue
         
-        try:
-            for neighbor, weight in graph[current_node].items():
-                tentative_g_score = g_scores[current_node] + weight
-                
-                if tentative_g_score < g_scores.get(neighbor, float("inf")):
-                    g_scores[neighbor] = tentative_g_score
-                    f_scores[neighbor] = tentative_g_score + heuristic_func(graph_proj, neighbor, end_node)
-                    previous_nodes[neighbor] = current_node
-                    hq.heappush(pq, (f_scores[neighbor], neighbor))
-                
-        except KeyError:
-                # protection against false neighours nodes, which can happen in real life data
+        # Iterate through all neighbors of the current node:
+        for neighbor_node, weight in graph[current_node].items():
+
+            # Calculate the new distance to the neighbor:
+            new_g_score = g_scores[current_node] + weight
+
+            try:
+                # If the new distance is shorter than the previously recorded distance:
+                if new_g_score < g_scores[neighbor_node]:
+
+                    # Update the shortest distance to the neighbor:
+                    g_scores[neighbor_node] = new_g_score
+
+                    previous_nodes[neighbor_node] = current_node
+
+                    # Update the previous node to reconstruct the path later:
+                    f_scores[neighbor_node] = new_g_score + heuristic_func(graph_proj, neighbor_node, end_node)
+
+                    # Push the neighbor with the updated distance into the priority queue:
+                    hq.heappush(pq, (f_scores[neighbor_node], neighbor_node))
+
+            except KeyError:
+            # protection against false neighours nodes, which can happen in real life data
                 pass
 
     # Reconstruct the shortest path from the end_node to the start_node
